55110c87f9279ae0d07b17e8e1d4d4240c8fcae7
Bonee94 <bonee94@yahoo.com>
2022-12-20 23:21:20 -0600

    Added generate html function and created manager html template function

---
+++ src/generateHTML.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+
+function generateHTML(managerArray, engineerArray, internArray) {
+
+    let idCounter = 2
+    managerHTML = managerToHTML(managerArray)
+    engineerHTML = engineerToHTML(engineerArray, idCounter)
+
+    idCounter = engineerArray.length + 2
+    internHTML = internToHTML(internArray, idCounter)
+
+}
+
+
+function managerToHTML(managerArray) {
+
+    if (managerArray.length > 0) {
+
+        let name = managerArray[0].memberName;
+        let id = '1';
+        let role = 'Manager';
+        let email = managerArray[0].memberEmail;
+        let officeNum = managerArray[0].managerOfficeNumber;
+
+        let html = `
+    <div class="card">
+                <div class="card-header">
+                    <h2 id="manager-name">${name}</h2>
+                    <h3 id="manager-title"><i class="fa fa-mug-hot"></i> ${role}</h3>
+                </div>
+                <div class="card-body">
+                    <ul>
+                        <li id="manager-id">ID: ${id}</li>
+                        <li id="manager-email">Email: 
+                        <a href="mailto:${email}">${email}</a>
+                        </li>
+                        <li id="manager-office-num">Office Number: ${officeNum}</li>
+                    </ul>
+                </div>
+            </div>
+    `
+
+        return html
+    }
+
+    return ''
+}
