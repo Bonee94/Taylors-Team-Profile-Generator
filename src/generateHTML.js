7f72c371b6f0f96e77454ae70f18589864088eff
Bonee94 <bonee94@yahoo.com>
2022-12-20 23:23:38 -0600

    Created engineer to html template function

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -45,3 +45,44 @@ function managerToHTML(managerArray) {
 
     return ''
 }
+
+
+function engineerToHTML(engineerArray, idCounter) {
+    if (engineerArray.length > 0) {
+        let html = ''
+
+        for (let index = 0; index < engineerArray.length; index++) {
+
+            let name = engineerArray[index].memberName;
+            let role = 'Engineer';
+            let email = engineerArray[index].memberEmail;
+            let github = engineerArray[index].memberGithubUsername;
+
+            html += `
+                <div class="card">
+                <div class="card-header">
+                    <h2 id="engineer-name">${name}</h2>
+                    <h3 id="engineer-title"><i class="fa fa-glasses"></i>
+                     ${role}</h3>
+                </div>
+                <div class="card-body">
+                    <ul>
+                        <li id="engineer-id">ID: ${idCounter}</li>
+                        <li id="engineer-email">Email: 
+                        <a href="mailto:${email}">${email}</a>
+                        </li>
+                        <li id="engineer-github">Github: 
+                        <a href="https://github.com/${github}">${github}</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+    `
+            idCounter++
+        }
+
+        return html;
+    }
+
+    return '';
+}
